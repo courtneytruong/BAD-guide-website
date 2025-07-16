5db31b59354cd965e6bb5b7657a6a989e726031f
courtneytruong <courtneybtruong@gmail.com>
2025-07-16 14:09:25 -0400

    file structure and routing set up

--- BAD-guild-website/src/App.jsx
+++ BAD-guild-website/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <h1>BAD guild website</h1>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Guides from "./pages/Guides";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/guides" element={<Guides />} />
+      </Routes>
+    </Router>
+  );
+}
