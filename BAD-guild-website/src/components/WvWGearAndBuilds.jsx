b32d81afdd094148ddc3b8941b5586adac5326ce
courtneytruong <courtneybtruong@gmail.com>
2025-07-29 16:22:29 -0400

    WvW gear and builds guide initial content added

--- BAD-guild-website/src/components/WvWGearAndBuilds.jsx
+++ BAD-guild-website/src/components/WvWGearAndBuilds.jsx
@@ -1,3 +1,44 @@
 export default function WvWGearAndBuilds() {
-  return <div>guide</div>;
+  return (
+    <div>
+      <div>
+        <p>
+          In WvW, gear becomes extremely important to survive. It's also very
+          different than PvE gear.
+        </p>
+        <p>Typically you'll use the following stats:</p>
+        <ul>
+          <li>Healing/support = Minstrel's</li>
+          <li>Power DPS = Maurader</li>
+          <li>Condition DPS = Celestial</li>
+        </ul>
+        <p>
+          These stats provide more vitality and sustainability with a slight dps
+          trade.
+        </p>
+      </div>
+      <div>
+        <h2>Builds</h2>
+        <p>
+          These websites provide various builds for WvW. In some cases, you'll
+          see different builds for roaming (solo or small group play) and zerg
+          (large groups).
+        </p>
+        <a href="https://gw2mists.com/builds">GW2 Mists</a>
+        <a href="https://metabattle.com/wiki/WvW">Metabattle</a>
+        <a href="https://guildjen.com/wvw-builds/">Guildjen</a>
+        <a href="https://metabattle.com/wiki/WvW_Roaming">
+          Metabattle Roaming Builds
+        </a>
+        <a href="https://snowcrows.com/builds/wvw">SnowCrows</a>
+      </div>
+      <div>
+        <h2>Group Comps</h2>
+        <p>
+          The ideal composition for each 5 person subgroup is to have 1 boon
+          firebrand, 1 healer, 3 dps/other support.
+        </p>
+      </div>
+    </div>
+  );
 }
