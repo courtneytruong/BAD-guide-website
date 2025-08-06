75bdf54dd135403af8f640f17aa8d8fb806c6f9a
courtneytruong <courtneybtruong@gmail.com>
2025-08-06 15:03:35 -0400

    reformatting completed. fixed a bug with strike mission guides where mobile top nav bar was no longer sticky after refactoring.

--- BAD-guild-website/src/components/MobileTopNavBar.jsx
+++ BAD-guild-website/src/components/MobileTopNavBar.jsx
@@ -3,20 +3,18 @@ import { VscDebugBreakpointLog } from "react-icons/vsc";
 export default function MobileTopNavBar({ navLinks }) {
   const flatLinks = navLinks.flatMap((section) => section.links);
   return (
-    <div>
-      <nav className="md:hidden sticky top-0 bg-gradient-to-r from-red-900 to-black text-neutral-50 overflow-x-auto whitespace-nowrap flex gap-4 px-4 py-2">
-        {flatLinks.map((link) => (
-          <a
-            key={link.id}
-            href={`#${link.id}`}
-            className="shrink-0 hover:text-neutral-300 transition"
-          >
-            <span className="flex items-center gap-x-2">
-              <VscDebugBreakpointLog /> {link.label}
-            </span>
-          </a>
-        ))}
-      </nav>
-    </div>
+    <nav className="md:hidden sticky top-0 z-50 bg-gradient-to-r from-red-900 to-black text-neutral-50 overflow-x-auto whitespace-nowrap flex gap-4 px-4 py-2">
+      {flatLinks.map((link) => (
+        <a
+          key={link.id}
+          href={`#${link.id}`}
+          className="shrink-0 hover:text-neutral-300 transition"
+        >
+          <span className="flex items-center gap-x-2">
+            <VscDebugBreakpointLog /> {link.label}
+          </span>
+        </a>
+      ))}
+    </nav>
   );
 }
