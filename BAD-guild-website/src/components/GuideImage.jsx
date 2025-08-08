2669e69f0270b216a827f6ebc5b39f2fdfe77747
courtneytruong <courtneybtruong@gmail.com>
2025-08-08 16:55:09 -0400

    changed close button icon on guideimage component.

--- BAD-guild-website/src/components/GuideImage.jsx
+++ BAD-guild-website/src/components/GuideImage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 export default function GuideImage({ src, alt, caption }) {
   const [isZoomed, setIsZoomed] = useState(false);
@@ -23,20 +24,7 @@ export default function GuideImage({ src, alt, caption }) {
               aria-label="Close zoomed image"
               className="absolute top-2 right-2 text-white bg-black bg-opacity-60 rounded-full p-2 hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-white z-60"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <IoClose size={30} />
             </button>
 
             {/* Zoomed image */}
