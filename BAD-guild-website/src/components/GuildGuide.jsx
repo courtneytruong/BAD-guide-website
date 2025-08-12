63eda9dcfc07252c8755a5931cb4fa462ac64e36
courtneytruong <courtneybtruong@gmail.com>
2025-08-12 16:29:08 -0400

    guild guide completed.

---
+++ BAD-guild-website/src/components/GuildGuide.jsx
@@ -0,0 +1,21 @@
+import navLinks from "../data/guildguidenavlinks";
+import guildGuideContent from "../data/guildguidecontent";
+import GuideRenderer from "./GuideRenderer";
+import BackToTopButton from "./BackToTopButton";
+import MobileTopNavBar from "./MobileTopNavBar";
+import SideBarNav from "./SideBarNav";
+
+export default function GuildGuide() {
+  return (
+    <div className="min-h-screen">
+      <MobileTopNavBar navLinks={navLinks} />
+      <div className="flex ">
+        <SideBarNav navLinks={navLinks} />
+        <main className="flex-1 p-10 space-y-10">
+          <GuideRenderer guide={guildGuideContent} />
+        </main>
+      </div>
+      <BackToTopButton />
+    </div>
+  );
+}
