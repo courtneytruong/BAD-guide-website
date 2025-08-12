a65495313aa2ca695a61091cd6e2502a80ef321e
courtneytruong <courtneybtruong@gmail.com>
2025-08-12 15:04:55 -0400

    wing 8 guide completed.

---
+++ BAD-guild-website/src/data/wing8navlinks.jsx
@@ -0,0 +1,13 @@
+const navLinks = [
+  {
+    header: "Wing 8 - Mount Balrior",
+    links: [
+      { id: "Pre-events", label: "Pre-events" },
+      { id: "Greer, the Blightbringer", label: "Greer, the Blightbringer" },
+      { id: "Decima, the Stormsinger", label: "Decima, the Stormsinger" },
+      { id: "Ura, the Steamshrieker", label: "Ura, the Steamshrieker" },
+    ],
+  },
+];
+
+export default navLinks;
