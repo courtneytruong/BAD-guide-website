b35b945db3592a4a343a871f3ab95fa070bfcb41
courtneytruong <courtneybtruong@gmail.com>
2025-08-15 11:29:36 -0400

    footer completed.

--- BAD-guild-website/src/layouts/Layout.jsx
+++ BAD-guild-website/src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen w-screen bg-neutral-900 text-white">
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }
